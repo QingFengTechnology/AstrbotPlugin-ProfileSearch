807e98d0516a1a8b95d974f85bc51aeff1333797
Zhalslar <zsl2936169201@gmail.com>
2025-04-16 23:27:01 +0800

    补充开盒信息

--- draw.py
+++ draw.py
@@ -9,7 +9,7 @@ import emoji
 
 RESOURCE_DIR: Path = Path(__file__).resolve().parent / "resource"
 FONT_PATH: Path = RESOURCE_DIR / "可爱字体.ttf"
-EMOJI_FONT_PATH: Path  = RESOURCE_DIR / "NotoColorEmoji.ttf"
+EMOJI_FONT_PATH: Path = RESOURCE_DIR / "NotoColorEmoji.ttf"
 
 FONT_SIZE = 35  # 字体大小
 TEXT_PADDING = 10  # 文本与边框的间距
@@ -21,13 +21,17 @@ CORNER_RADIUS = 30  # 圆角大小
 cute_font = ImageFont.truetype(FONT_PATH, FONT_SIZE)
 emoji_font = ImageFont.truetype(EMOJI_FONT_PATH, FONT_SIZE)
 
+
 def create_image(avatar: bytes, reply: list) -> bytes:
     reply_str = "\n".join(reply)
     # 创建临时图片计算文本的宽高，得到最图片高度
-    temp_img = Image.new('RGB', (1, 1))
+    temp_img = Image.new("RGBA", (1, 1))
     temp_draw = ImageDraw.Draw(temp_img)
     text_bbox = temp_draw.textbbox((0, 0), reply_str, font=cute_font)
-    text_width, text_height = int(text_bbox[2] - text_bbox[0]), int(text_bbox[3] - text_bbox[1])
+    text_width, text_height = (
+        int(text_bbox[2] - text_bbox[0]),
+        int(text_bbox[3] - text_bbox[1]),
+    )
     img_height = text_height + 2 * TEXT_PADDING
     # 调整头像为与文本高度相同的大小，得到图片的宽度
     avatar_img = Image.open(BytesIO(avatar))
@@ -35,34 +39,34 @@ def create_image(avatar: bytes, reply: list) -> bytes:
     avatar_img = avatar_img.resize((avatar_size, avatar_size))
     img_width = avatar_img.width + text_width + 2 * TEXT_PADDING
     # 头像圆角后粘贴到图片左侧,垂直居中
-    img = Image.new('RGB', (img_width, img_height), color=(255, 255, 255))
-    mask = Image.new('L', (avatar_size, avatar_size), 0)
+    img = Image.new("RGBA", (img_width, img_height), color=(255, 255, 255, 255))
+    mask = Image.new("L", (avatar_size, avatar_size), 0)
     draw = ImageDraw.Draw(mask)
-    draw.rounded_rectangle([(0, 0), (avatar_size, avatar_size)], CORNER_RADIUS, fill=255)
+    draw.rounded_rectangle(
+        [(0, 0), (avatar_size, avatar_size)], CORNER_RADIUS, fill=255
+    )
     avatar_img.putalpha(mask)
     img.paste(avatar_img, (0, (img_height - avatar_size) // 2), mask)
     # 绘制文本到图片右侧
-    _draw_multi(img, reply_str,avatar_img.width + TEXT_PADDING, TEXT_PADDING)
+    _draw_multi(img, reply_str, avatar_img.width + TEXT_PADDING, TEXT_PADDING)
     # 绘制一个随机颜色的边框
     border_color = (
         random.randint(*BORDER_COLOR_RANGE),
         random.randint(*BORDER_COLOR_RANGE),
-        random.randint(*BORDER_COLOR_RANGE)
+        random.randint(*BORDER_COLOR_RANGE),
     )
     border_img = Image.new(
-        mode='RGB',
+        mode="RGBA",
         size=(img_width + BORDER_THICKNESS * 2, img_height + BORDER_THICKNESS * 2),
-        color=border_color
+        color=border_color,
     )
     border_img.paste(img, (BORDER_THICKNESS, BORDER_THICKNESS))
 
     img_byte_arr = io.BytesIO()
-    border_img.save(img_byte_arr, format='PNG')
+    border_img.save(img_byte_arr, format="PNG")
     img_byte_arr = img_byte_arr.getvalue()
 
     return img_byte_arr
-
-
 
 
 def _draw_multi(img, text, text_x=10, text_y=10):
@@ -76,15 +80,20 @@ def _draw_multi(img, text, text_x=10, text_y=10):
 
     # 遍历每一行文本
     for line in lines:
-        line_color = (random.randint(0, 128), random.randint(0, 128), random.randint(0, 128))
+        line_color = (
+            random.randint(0, 128),
+            random.randint(0, 128),
+            random.randint(0, 128),
+            random.randint(240, 255),
+        )
         current_x = text_x
         for char in line:
             if char in emoji.EMOJI_DATA:
-                draw.text((current_x, current_y + 10), char, font=emoji_font, fill=line_color)
+                draw.text((current_x, current_y + 10), char, font=emoji_font,fill=line_color)
+                bbox = emoji_font.getbbox(char)
             else:
                 draw.text((current_x, current_y), char, font=cute_font, fill=line_color)
-
-            bbox = cute_font.getbbox(char)
+                bbox = cute_font.getbbox(char)
             current_x += bbox[2] - bbox[0]
         current_y += 40
     return img
--- main.py
+++ main.py
@@ -1,49 +1,326 @@
+from datetime import datetime
 
-from astrbot.api.all import *
+import aiohttp
+from astrbot.api.star import Context, Star, register
+from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
+    AiocqhttpMessageEvent,
+)
 from .draw import create_image
-from .utils import get_avatar, transform
 import astrbot.api.message_components as Comp
 from astrbot import logger
 from astrbot.api.event import filter
 
 
-
-@register("盒", "Zhalslar", "开盒插件", "1.0.2")
+@register(
+    "astrbot_plugin_box",
+    "Zhalslar",
+    "开盒插件",
+    "1.0.3",
+    "https://github.com/Zhalslar/astrbot_plugin_box",
+)
 class Box(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
     @filter.command("盒", alias={"开盒"})
-    async def box(self, event: AstrMessageEvent):
-        """/盒@user"""
+    async def box(self, event: AiocqhttpMessageEvent):
+        """/盒@某人"""
         messages = event.get_messages()
         send_id = event.get_sender_id()
         self_id = event.get_self_id()
-        target_user_id = next((str(seg.qq) for seg in messages if (isinstance(seg, Comp.At)) and str(seg.qq) != self_id), send_id)
+        target_id = next(
+            (
+                str(seg.qq)
+                for seg in messages
+                if (isinstance(seg, Comp.At)) and str(seg.qq) != self_id
+            ),
+            send_id,
+        )
         group_id = event.get_group_id()
+        client = event.bot
         try:
-            if event.get_platform_name() == "aiocqhttp":
-                from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import AiocqhttpMessageEvent
-                assert isinstance(event, AiocqhttpMessageEvent)
-                client = event.bot
+            stranger_info = await client.get_stranger_info(
+                user_id=int(target_id), no_cache=True
+            )
+            print(stranger_info)
+            member_info = await client.get_group_member_info(
+                user_id=int(target_id), group_id=int(group_id)
+            )
 
-                stranger_payloads = {"user_id": target_user_id}
-                member_payloads = {"user_id": target_user_id, "group_id": group_id,}
-                stranger_info: dict = await client.api.call_action('get_stranger_info', **stranger_payloads)
-                member_info: dict = await client.api.call_action('get_group_member_info', **member_payloads)
-
-                avatar: bytes = await get_avatar(target_user_id)
-                reply: list = transform(stranger_info, member_info)
-                image: bytes = create_image(avatar, reply)
-                chain = [Comp.Image.fromBytes(image)]
-                yield event.chain_result(chain)
+            avatar: bytes = await self.get_avatar(target_id)
+            reply: list = self.transform(stranger_info, member_info)  # type: ignore
+            image: bytes = create_image(avatar, reply)
+            chain = [Comp.Image.fromBytes(image)]
+            yield event.chain_result(chain)  # type: ignore
 
         except Exception as e:
             logger.error(f"开盒出错: {e}")
 
+    def transform(self, info: dict, info2: dict) -> list:
+        replay = []
 
+        if user_id := info2.get("user_id"):
+            replay.append(f"Q号：{user_id}")
 
+        if nickname := info2.get("nickname"):
+            replay.append(f"昵称：{nickname}")
 
+        if card := info2.get("card"):
+            replay.append(f"群昵称：{card}")
 
+        if title := info2.get("title"):
+            replay.append(f"头衔：{title}")
 
+        # if info.get('status', False) and int(info['status']) != 20:
+        # replay.append(f"状态：{get_state(info['uin'])}")
 
+        sex = info.get("sex")
+        if sex == "male":
+            replay.append("性别：男孩纸")
+        elif sex == "female":
+            replay.append("性别：女孩纸")
+
+        if (
+            info.get("birthday_year")
+            and info.get("birthday_month")
+            and info.get("birthday_day")
+        ):
+            replay.append(
+                f"诞辰：{info['birthday_year']}-{info['birthday_month']}-{info['birthday_day']}"
+            )
+            replay.append(
+                f"星座：{self.get_constellation(int(info['birthday_month']), int(info['birthday_day']))}"
+            )
+            replay.append(
+                f"生肖：{self.get_zodiac(int(info['birthday_year']), int(info['birthday_month']), int(info['birthday_day']))}"
+            )
+
+        if age := info.get("age"):
+            replay.append(f"年龄：{age}岁")
+
+        if phoneNum := info.get("phoneNum"):
+            if phoneNum != "-":
+                replay.append(f"电话：{phoneNum}")
+
+        if eMail := info.get("eMail", False):
+            if eMail != "-":
+                replay.append(f"邮箱：{eMail}")
+
+        if postCode := info.get("postCode", False):
+            replay.append(f"邮编：{postCode}")
+
+        if country := info.get("country"):
+            replay.append(f"现居：{country}")
+        if province := info.get("province"):
+            replay[-1] += f"-{province}"
+        if city := info.get("city"):
+            replay[-1] += f"-{city}"
+
+        if homeTown := info.get("homeTown"):
+            if homeTown != "0-0-0":
+                replay.append(f"来自：{self.parse_home_town(homeTown)}")
+
+        if info.get("address", False):
+            replay.append(f"地址：{info['address']}")
+
+        if kBloodType := info.get("kBloodType"):
+            replay.append(f"血型：{self.get_blood_type(int(kBloodType))}")
+
+        if (
+            makeFriendCareer := info.get("makeFriendCareer")
+        ) and makeFriendCareer != "0":
+            replay.append(f"职业：{self.get_career(int(makeFriendCareer))}")
+
+        if remark := info.get("remark"):
+            replay.append(f"备注：{remark}")
+
+        if labels := info.get("labels"):
+            replay.append(f"标签：{labels}")
+
+        if info2.get("unfriendly"):
+            replay.append("不良记录：有")
+
+        if info2.get("is_robot"):
+            replay.append("是否为bot: 是")
+
+        if info.get("is_vip"):
+            replay.append("VIP：已开")
+
+        if info.get("is_years_vip"):
+            replay.append("年费VIP：已开")
+
+        if int(info.get("vip_level", 0)) != 0:
+            replay.append(f"VIP等级：{info['vip_level']}")
+
+        if int(info.get("login_days", 0)) != 0:
+            replay.append(f"连续登录天数：{info['login_days']}")
+
+        if level := info2.get("level"):
+            replay.append(f"群等级：{int(level)}级")
+
+        if join_time := info2.get("join_time"):
+            replay.append(
+                f"加群时间：{datetime.fromtimestamp(join_time).strftime('%Y-%m-%d')}"
+            )
+
+        if qqLevel := info.get("qqLevel"):
+            replay.append(f"QQ等级：{self.qqLevel_to_icon(int(qqLevel))}")
+
+        if reg_time := info.get("reg_time"):
+            replay.append(
+                f"注册时间：{datetime.fromtimestamp(reg_time).strftime('%Y年')}"
+            )
+
+        if long_nick := info.get("long_nick"):
+            long_nick_lines = [
+                info["long_nick"][i : i + 15] for i in range(0, len(long_nick), 15)
+            ]
+            replay.append(f"签名：{long_nick_lines[0]}")
+            for line in long_nick_lines[1:]:
+                replay.append(line)
+
+        return replay
+
+    @staticmethod
+    def qqLevel_to_icon(level: int) -> str:
+        icons = ["👑", "🌞", "🌙", "⭐"]
+        levels = [64, 16, 4, 1]
+        result = ""
+        original_level = level
+        for icon, lvl in zip(icons, levels):
+            count, level = divmod(level, lvl)
+            result += icon * count
+        result += f"({original_level})"
+        return result
+
+    @staticmethod
+    async def get_avatar(user_id: str) -> bytes:
+        avatar_url = f"https://q4.qlogo.cn/headimg_dl?dst_uin={user_id}&spec=640"
+        try:
+            async with aiohttp.ClientSession() as client:
+                response = await client.get(avatar_url, timeout=10)
+                response.raise_for_status()
+                return await response.read()
+        except Exception as e:
+            logger.error(f"下载头像失败: {e}")
+            return b""
+
+    @staticmethod
+    def get_constellation(month: int, day: int) -> str:
+        constellations = {
+            "白羊座": ((3, 21), (4, 19)),
+            "金牛座": ((4, 20), (5, 20)),
+            "双子座": ((5, 21), (6, 20)),
+            "巨蟹座": ((6, 21), (7, 22)),
+            "狮子座": ((7, 23), (8, 22)),
+            "处女座": ((8, 23), (9, 22)),
+            "天秤座": ((9, 23), (10, 22)),
+            "天蝎座": ((10, 23), (11, 21)),
+            "射手座": ((11, 22), (12, 21)),
+            "摩羯座": ((12, 22), (1, 19)),
+            "水瓶座": ((1, 20), (2, 18)),
+            "双鱼座": ((2, 19), (3, 20)),
+        }
+
+        for constellation, (
+            (start_month, start_day),
+            (end_month, end_day),
+        ) in constellations.items():
+            if (month == start_month and day >= start_day) or (
+                month == end_month and day <= end_day
+            ):
+                return constellation
+            # 特别处理跨年星座
+            if start_month > end_month:
+                if (month == start_month and day >= start_day) or (
+                    month == end_month + 12 and day <= end_day
+                ):
+                    return constellation
+        return f"星座{month}-{day}"
+
+    @staticmethod
+    def get_zodiac(year: int, month: int, day: int) -> str:
+        # 2024年是龙年，以此为基准
+        base_year = 2024
+        zodiacs = [
+            "龙🐉",
+            "蛇🐍",
+            "马🐎",
+            "羊🐏",
+            "猴🐒",
+            "鸡🐔",
+            "狗🐕",
+            "猪🐖",
+            "鼠🐀",
+            "牛🐂",
+            "虎🐅",
+            "兔🐇",
+        ]
+        # 如果输入的日期在2月4日之前，生肖年份应该是上一年
+        if (month == 1) or (month == 2 and day < 4):
+            zodiac_year = year - 1
+        else:
+            zodiac_year = year
+
+        zodiac_index = (zodiac_year - base_year) % 12
+        return zodiacs[zodiac_index]
+
+    @staticmethod
+    def get_career(num: int) -> str:
+        career = {
+            1: "计算机/互联网/通信",
+            2: "生产/工艺/制造",
+            3: "医疗/护理/制药",
+            4: "金融/银行/投资/保险",
+            5: "商业/服务业/个体经营",
+            6: "文化/广告/传媒",
+            7: "娱乐/艺术/表演",
+            8: "律师/法务",
+            9: "教育/培训",
+            10: "公务员/行政/事业单位",
+            11: "模特",
+            12: "空姐",
+            13: "学生",
+            14: "其他职业",
+        }
+        return career.get(num, f"职业{num}")
+
+    @staticmethod
+    def get_blood_type(num: int) -> str:
+        blood_types = {1: "A型", 2: "B型", 3: "O型", 4: "AB型", 5: "其他血型"}
+        return blood_types.get(num, f"血型{num}")
+
+    @staticmethod
+    def parse_home_town(home_town_code: str) -> str:
+        # 国家代码映射表
+        country_map = {
+            "49": "中国",
+            "250": "俄罗斯",
+            "222": "特里尔",
+            "217": "法国",
+            "233": "美国",
+        }
+        # 中国省份（包括直辖市）代码映射表，由于不是一一对应，效果不佳
+        province_map = {
+            "98": "北京",
+            "99": "天津/辽宁",
+            "100": "冀/沪/吉",
+            "101": "苏/豫/晋/黑/渝",
+            "102": "浙/鄂/蒙/川",
+            "103": "皖/湘/黔/陕",
+            "104": "闽/粤/滇/甘/台",
+            "105": "赣/桂/藏/青/港",
+            "106": "鲁/琼/陕/宁/澳",
+            "107": "新疆",
+        }
+
+        country_code, province_code, _ = home_town_code.split("-")
+        country = country_map.get(country_code, f"外国{country_code}")
+
+        reply = country
+
+        if country_code == "49" and province_code != "0":  # 解析中国省份
+            province = province_map.get(province_code, f"{province_code}省")
+            #province = f"{province_code}省"
+            reply = province
+        return reply
